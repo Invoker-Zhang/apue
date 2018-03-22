9781e5ea3b409edce1396c7443e83cd133cbc29d
Invoker <InvokerZhang@outlook.com>
2018-03-22 20:04:20 +0800

    chap5

---
+++ execises/src/exe5_1.c
@@ -0,0 +1,11 @@
+#include "ourhdr.h"
+
+int main(){
+	int c;
+	while( (c = getc(stdin)) != EOF) 
+		if(putc(c, stdout) == EOF)
+			err_sys("output error");
+	if(ferror(stdin))
+		err_sys("input error");
+	exit(0);
+}
